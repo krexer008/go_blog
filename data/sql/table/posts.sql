281d6088478048c916fb48ae5c56b391669735df
krexer008 <y.fedor86@mail.ru>
2023-03-31 01:24:45 +0300

    31.03.2023 1:24

--- data/sql/table/posts.sql
+++ data/sql/table/posts.sql
@@ -4,17 +4,20 @@ SHOW DATABASES;
 CREATE DATABASE blog;
 CREATE TABLE post
 (
-    post_id PRIMARY KEY INT NOT NULL AUTO_INCREMENT,
+    post_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     subtitle VARCHAR (255),
     category VARCHAR (255),
     author VARCHAR (255),
     author_url VARCHAR (255),
     publish_date VARCHAR (255),
+    image_modifier VARCHAR (255),
     image_url VARCHAR (255),
-    featured TINYINT(1) DEFAULT 0,
+    featured TINYINT(1) DEFAULT 0
 )
     ENGINE = InnoDB
     CHARACTER SET = utf8mb4
-    COLLATE utf8mb5_unicode_ci
+    COLLATE utf8mb4_unicode_ci
 ;
+USE blog;
+SHOW TABLES;
