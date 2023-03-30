da3cc715531856a3cc96b1f4b375958a365ef851
konstr17 <cheezzz@list.ru>
2023-03-30 16:25:56 +0300

    30.03.2023 16:25

---
+++ data/sql/table/posts.sql
@@ -0,0 +1,20 @@
+SHOW DATABASES;
+DROP DATABASE blog;
+SHOW DATABASES;
+CREATE DATABASE blog;
+CREATE TABLE post
+(
+    post_id PRIMARY KEY INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    subtitle VARCHAR (255),
+    category VARCHAR (255),
+    author VARCHAR (255),
+    author_url VARCHAR (255),
+    publish_date VARCHAR (255),
+    image_url VARCHAR (255),
+    featured TINYINT(1) DEFAULT 0,
+)
+    ENGINE = InnoDB
+    CHARACTER SET = utf8mb4
+    COLLATE utf8mb5_unicode_ci
+;
