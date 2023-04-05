0ee3524b9f9910ef42a8ecf75ac4a5d32e6430e8
konstr17 <cheezzz@list.ru>
2023-04-05 12:00:23 +0300

    04.04.2023. 16.01

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -1,117 +1,117 @@
 package main
 
 import (
-    "html/template" // Модуль отвечает за шаблонизацию html страниц
-    "log"
-    "net/http"
+	"html/template" // Модуль отвечает за шаблонизацию html страниц
+	"log"
+	"net/http"
+	"strings"
 
-    "github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx"
 )
 
 type indexPageData struct {
-    FeaturedPosts []featuredPostData
-    RecentPosts   []recentPostData
+	FeaturedPosts []featuredPostData
+	RecentPosts   []recentPostData
 }
 
 type postPageData struct {
-    Title          string `db:"title"`
-    Subtitle       string `db:"subtitle"`
-    PostImage      string `db:"post_image_url"`
-    Text           string `db:"text"`
-    PostParagraphs []string
+	Title          string `db:"title"`
+	Subtitle       string `db:"subtitle"`
+	PostImage      string `db:"post_image_url"`
+	Text           string `db:"text"`
+	PostParagraphs []string
 }
 
 type featuredPostData struct { //featuredPostData
-    Title        string `db:"title"`
-    Subtitle     string `db:"subtitle"`
-    PostCategory string `db:"category"`
-    ImgModifier  string `db:"image_modifier"`
-    Author       string `db:"author"`
-    AuthorImg    string `db:"author_url"`
-    PublishDate  string `db:"publish_date"`
+	Title        string `db:"title"`
+	Subtitle     string `db:"subtitle"`
+	PostCategory string `db:"category"`
+	ImgModifier  string `db:"image_modifier"`
+	Author       string `db:"author"`
+	AuthorImg    string `db:"author_url"`
+	PublishDate  string `db:"publish_date"`
 }
 
 type recentPostData struct {
-    Title         string `db:"title"`
-    Subtitle      string `db:"subtitle"`
-    PostCategory  string `db:"category"`
-    PostThumbnail string `db:"post_thumbnail_url"`
-    Author        string `db:"author"`
-    AuthorImg     string `db:"author_url"`
-    PublishDate   string `db:"publish_date"`
+	Title         string `db:"title"`
+	Subtitle      string `db:"subtitle"`
+	PostCategory  string `db:"category"`
+	PostThumbnail string `db:"post_thumbnail_url"`
+	Author        string `db:"author"`
+	AuthorImg     string `db:"author_url"`
+	PublishDate   string `db:"publish_date"`
 }
 
 func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-        featuredPosts, err := getFeaturedPosts(db)
-        if err != nil {
-            http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-            log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-            return                                      // Завершение функции
-        }
+	return func(w http.ResponseWriter, r *http.Request) {
+		featuredPosts, err := getFeaturedPosts(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+			return                                      // Завершение функции
+		}
 
-        recentPosts, err := getRecentPosts(db)
-        if err != nil {
-            http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-            log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-            return                                      // Завершение функции
-        }
+		recentPosts, err := getRecentPosts(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+			return                                      // Завершение функции
+		}
 
-        ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
-        if err != nil {
-            http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-            log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-            return
-        }
+		ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+			return
+		}
 
-        data := indexPageData{
-            FeaturedPosts: featuredPosts,
-            RecentPosts:   recentPosts,
-        }
+		data := indexPageData{
+			FeaturedPosts: featuredPosts,
+			RecentPosts:   recentPosts,
+		}
 
-        err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
-        if err != nil {
-            http.Error(w, "Internal Server Error", 500)
-            log.Println(err.Error())
-            return
-        }
+		err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err.Error())
+			return
+		}
 
-        log.Println("Request completed succesfully")
+		log.Println("Request completed succesfully")
 
-    }
+	}
 }
 
-func post(db *sqlx.DB, post_id) func(w http.ResponseWriter, r *http.Request) {
-    return func(w http.ResponseWriter, r *http.Request) {
-        postPage, err := getPostPage(db, post_id)
-        if err != nil {
-            http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-            log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-            return                                      // Завершение функции
-        }
+func post(db *sqlx.DB, post_id int) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		postPage, err := getPostPage(db, post_id)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+			return                                      // Завершение функции
+		}
 
-        ts, err := template.ParseFiles("pages/post.html") // Главная страница блога
-        if err != nil {
-            http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-            log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-            return
-        }
+		ts, err := template.ParseFiles("pages/post.html") // Главная страница блога
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+			log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+			return
+		}
 
+		err = ts.Execute(w, postPage) // Заставляем шаблонизатор вывести шаблон в тело ответа
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err.Error())
+			return
+		}
 
-        err = ts.Execute(w, postPage) // Заставляем шаблонизатор вывести шаблон в тело ответа
-        if err != nil {
-            http.Error(w, "Internal Server Error", 500)
-            log.Println(err.Error())
-            return
-        }
-
-        log.Println("Request completed succesfully")
-    }
+		log.Println("Request completed succesfully")
+	}
 }
 
-func getfeaturedPosts(db *sqlx.DB) ([]featuredPostData, error) {
-    // Составляем SQL-запрос для получения записей для секции featured-posts
-    const query = `
+func getFeaturedPosts(db *sqlx.DB) ([]featuredPostData, error) {
+	// Составляем SQL-запрос для получения записей для секции featured-posts
+	const query = `
     SELECT
         title,
         subtitle,
@@ -125,18 +125,18 @@ func getfeaturedPosts(db *sqlx.DB) ([]featuredPostData, error) {
     WHERE featured = 1
     `
 
-    var featuredPostsData []featuredPostData // Заранее объявляем массив с результирующей информацией
-    err := db.Select(&posts, query)          // Делаем запрос в базу данных // Select позволяет прочитать несколько строк
-    if err != nil {                          // Проверяем, что запрос в базу данных не завершился с ошибкой
-        return nil, err
-    }
+	var featuredPostsData []featuredPostData    // Заранее объявляем массив с результирующей информацией
+	err := db.Select(&featuredPostsData, query) // Делаем запрос в базу данных // Select позволяет прочитать несколько строк
+	if err != nil {                             // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return nil, err
+	}
 
-    return featuredPostsData, nil
+	return featuredPostsData, nil
 }
 
 func getRecentPosts(db *sqlx.DB) ([]recentPostData, error) {
-    // Составляем SQL-запрос для получения записей для секции featured-posts
-    const query = `
+	// Составляем SQL-запрос для получения записей для секции featured-posts
+	const query = `
     SELECT
         title,
         subtitle,
@@ -144,24 +144,24 @@ func getRecentPosts(db *sqlx.DB) ([]recentPostData, error) {
         author,
         author_url,
         publish_date,
-        image_url
+        post_thumbnail_url
     FROM
         post
     WHERE featured = 0
     `
 
-    var recentPostsData []recentPostData //recentPostData      // Заранее объявляем массив с результирующей информацией
-    err := db.Select(&posts, query)      // Делаем запрос в базу данных
-    if err != nil {                      // Проверяем, что запрос в базу данных не завершился с ошибкой
-        return nil, err
-    }
+	var recentPostsData []recentPostData      //recentPostData      // Заранее объявляем массив с результирующей информацией
+	err := db.Select(&recentPostsData, query) // Делаем запрос в базу данных
+	if err != nil {                           // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return nil, err
+	}
 
-    return recentPostData, nil
+	return recentPostsData, nil
 }
 
 func getPostPage(db *sqlx.DB, post_id int) (postPageData, error) {
-    // Составляем SQL-запрос для получения записей для секции featured-posts
-    const query = `
+	// Составляем SQL-запрос для получения записей для секции featured-posts
+	const query = `
     SELECT
         title,
         subtitle,
@@ -173,17 +173,14 @@ func getPostPage(db *sqlx.DB, post_id int) (postPageData, error) {
         post_id = ?
     `
 
-    var pageData postPageData
+	var pageData postPageData
 
-    err := db.Select(&posts, query) // Делаем запрос в базу данных
-    if err != nil {                 // Проверяем, что запрос в базу данных не завершился с ошибкой
-        return nil, err
-    }
+	err := db.Get(&pageData, query, post_id) // Делаем запрос в базу данных
+	if err != nil {                          // Проверяем, что запрос в базу данных не завершился с ошибкой
+		return pageData, err
+	}
 
-    return posts, nil
-}
+	pageData.PostParagraphs = strings.Split(pageData.Text, "\n")
 
-func postParagraphs() []string {
-
-    return []string{}
+	return pageData, nil
 }
