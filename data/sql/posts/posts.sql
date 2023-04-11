166b39fa3f395d7fad75e3fb7b6001838d384d30
konstr17 <cheezzz@list.ru>
2023-04-11 15:58:10 +0300

    07.04.2023 15.19

--- data/sql/posts/posts.sql
+++ data/sql/posts/posts.sql
@@ -1,36 +1,22 @@
-INSERT INTO post (
-        title,
-        subtitle,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'The Road Ahead',
+INSERT INTO post (title,
+                  subtitle,
+                  category,
+                  image_url,
+                  author,
+                  author_url,
+                  publish_date,
+                  text,
+                  featured)
+VALUES ('The Road Ahead',
         'The road ahead might be paved - it might not be.',
+        '',
         'static/img/the_road_ahead.jpg',
         'Mat Vogels',
         'static/img/mat_vogels.jpg',
         'September 25, 2015',
         '"Dark spruce forest frowned on either side the frozen waterway. The trees had been stripped by a recent wind of their white covering of frost, and they seemed to lean towards each other, black and ominous, in the fading light. A vast silence reigned over the land. The land itself was a desolation, lifeless, without movement, so lone and cold that the spirit of it was not even that of sadness. There was a hint in it of laughter, but of a laughter more terrible than any sadness—a laughter that was mirthless as the smile of the sphinx, a laughter cold as the frost and partaking of the grimness of infallibility. It was the masterful and incommunicable wisdom of eternity laughing at the futility of life and the effort of life. It was the Wild, the savage, frozen-hearted Northland Wild.\nBut there was life, abroad in the land and defiant. Down the frozen waterway toiled a string of wolfish dogs. Their bristly fur was rimed with frost. Their breath froze in the air as it left their mouths, spouting forth in spumes of vapour that settled upon the hair of their bodies and formed into crystals of frost. Leather harness was on the dogs, and leather traces attached them to a sled which dragged along behind. The sled was without runners. It was made of stout birch-bark, and its full surface rested on the snow. The front end of the sled was turned up, like a scroll, in order to force down and under the bore of soft snow that surged like a wave before it. On the sled, securely lashed, was a long and narrow oblong box. There were other things on the sled—blankets, an axe, and a coffee-pot and frying-pan; but prominent, occupying most of the space, was the long and narrow oblong box.\nIn advance of the dogs, on wide snowshoes, toiled a man. At the rear of the sled toiled a second man. On the sled, in the box, lay a third man whose toil was over,—a man whom the Wild had conquered and beaten down until he would never move nor struggle again. It is not the way of the Wild to like movement. Life is an offence to it, for life is movement; and the Wild aims always to destroy movement. It freezes the water to prevent it running to the sea; it drives the sap out of the trees till they are frozen to their mighty hearts; and most ferociously and terribly of all does the Wild harry and crush into submission man—man who is the most restless of life, ever in revolt against the dictum that all movement must in the end come to the cessation of movement.\nBut at front and rear, unawed and indomitable, toiled the two men who were not yet dead. Their bodies were covered with fur and soft-tanned leather. Eyelashes and cheeks and lips were so coated with the crystals from their frozen breath that their faces were not discernible. This gave them the seeming of ghostly masques, undertakers in a spectral world at the funeral of some ghost. But under it all they were men, penetrating the land of desolation and mockery and silence, puny adventurers bent on colossal adventure, pitting themselves against the might of a world as remote and alien and pulseless as the abysses of space.\n',
-        1
-    );
-INSERT INTO post (
-        title,
-        subtitle,
-        category,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'From Top Dow',
+        1),
+       ('From Top Dow',
         'Once a year, go someplace you never been before.',
         'Adventure',
         'static/img/from_top_down.jpg',
@@ -38,125 +24,58 @@ VALUES (
         'static/img/william_wong.jpg',
         'September 25, 2015',
         '',
-        1
-    );
-INSERT INTO post (
-        title,
-        subtitle,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'Still Standing Tall',
+        1),
+       ('Still Standing Tall',
         'Life begins at the end of your comfort zone.',
+        '',
         'static/img/still_standing_tall.jpg',
         'William Wong',
         'static/img/william_wong.jpg',
         '9/25/2015',
         '',
-        0
-    );
-INSERT INTO post (
-        title,
-        subtitle,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'Sunny Side Up',
+        0),
+       ('Sunny Side Up',
         'No place is ever as bad as they tell you it''s going to be.',
+        '',
         'static/img/sunny_side_up.jpg',
         'Mat Vogels',
         'static/img/mat_vogels.jpg',
         '9/25/2015',
         '',
-        0
-    );
-INSERT INTO post (
-        title,
-        subtitle,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'Water Falls',
+        0),
+       ('Water Falls',
         'We travel not to escape life, but for life not to escape us.',
+        '',
         'static/img/water_falls.jpg',
         'Mat Vogels',
         'static/img/mat_vogels.jpg',
         '9/25/2015',
         '',
-        0
-    );
-INSERT INTO post (
-        title,
-        subtitle,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'Through the Mist',
+        0),
+       ('Through the Mist',
         'Travel makes you see what a tiny place you occupy in the world.',
+        '',
         'static/img/through_the_mist.jpg',
         'William Wong',
         'static/img/william_wong.jpg',
         '9/25/2015',
         '',
-        0
-    );
-INSERT INTO post (
-        title,
-        subtitle,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'Awaken Early',
+        0),
+       ('Awaken Early',
         'Not all those who wander are lost.',
+        '',
         'static/img/awaken_early.jpg',
         'Mat Vogels',
         'static/img/mat_vogels.jpg',
         '9/25/2015',
         '',
-        0
-    );
-INSERT INTO post (
-        title,
-        subtitle,
-        image_url,
-        author,
-        author_url,
-        publish_date,
-        text,
-        featured
-    )
-VALUES (
-        'Try it Always',
+        0),
+       ('Try it Always',
         'The world is a book, and those who do not travel read only one page.',
+        '',
         'static/img/try_it_always.jpg',
         'Mat Vogels',
         'static/img/mat_vogels.jpg',
         '9/25/2015',
         '',
-        0
-    );
+        0);
