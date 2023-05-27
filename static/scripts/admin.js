d7568f62b731df4870137af62e559cb38ce2d51c
krexer008 <y.fedor86@mail.ru>
2023-05-28 00:12:18 +0300

    0/12

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -29,20 +29,24 @@ window.addEventListener('DOMContentLoaded', function () {
 
     const titleField = this.document.getElementById(keyTitle);
     const subtitleField = this.document.getElementById(keySubtitle);
-    const authorNameField = this.document.getElementById(keyAuthorName);/*
+    const authorNameField = this.document.getElementById(keyAuthorName);
+
+
+    /*
     const authorImageField = this.document.getElementById(keyAuthorImage);
     const dateField = this.document.getElementById(keyPublishDate);
     const largeImageField = this.document.getElementById(keyLargeImage);
     const shortImageField = this.document.getElementById(keyShortImage);
-    const contentField = this.document.getElementById(keyContent);*/
+    const contentField = this.document.getElementById(keyContent);
+    */
 
     var validFieldsValid = true;
 
     fieldViewHandler(titleField);
-    fieldViewHandler(subtitleField);
+    /*fieldViewHandler(subtitleField);
     fieldViewHandler(authorNameField);
-    
-    
+*/
+
     /*
     fieldViewHandler(dateField);
     
@@ -67,56 +71,51 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function fieldBlured(e) {
         e.target.classList.remove('form__field_focused');
-        if (e.target.value === "") {
+        if (e.target.value == "") {
             e.target.classList.remove('form__field_full');
         } else {
             e.target.classList.add('form__field_full');
-            this.alert('asd');
-           /* formData.set(e.target.id, e.target.value);
-            
-            /*updatePreview();*/
-        }
-    }
+            formData.set(e.target.id, e.target.value);
+            updatePreview();
+        };
 
-    function updatePreview() {
-        if (formData.get(keyTitle) !== "") {
-            document.getElementById(previewPageTitle).textContent = formData.get(keyTitle);
-            document.getElementById(previewPostCardTitle).textContent = formData.get(keyTitle);
-        };
-        if (formData.get(keySubtitle) !== "") {
-            document.getElementById(previewPageSubitle).textContent = formData.get(keySubtitle);
-            document.getElementById(previewPostCardSubtitle).textContent = formData.get(keySubtitle);
-        };
-        if (formData.get(keyAuthorName) !== "") {
-            document.getElementById(previewAuthorName).textContent = formData.get(keyAuthorName);
-        };
-        if (formData.get(keyPublishDate) !== "") {
-            document.getElementById(previewPublishDate).textContent = formData.get(keyPublishDate);
-        };
-        if (formData.get(keyContent) !== "") {
-            document.getElementById(previewPublishDate).textContent = formData.get(keyContent);
-        };
-        /*
-        
-                [keyPublishDate, ""],
-                    [keyLargeImage, ""],
-                    [keyShortImage, ""],
-                    [keyContent, ""],
-        */
     }
-    /*
-        function fieldKeyUpHandler(e) {
-            e.addEventListener('keyup', fieldKeyUp);
-        }
     
-        function fieldKeyUp(e) {
-            const incorrectField = document.querySelector('#status-incorect');
-            const needcheckField = document.querySelector('#status-need_check');
-            incorrectField.target.classList.add('hide_element');
-            needcheckField.target.classList.add('hide_element');
-    
-    
-        }
+        function updatePreview() {
+            if (formData.get(keyTitle) != "") {
+                console.log(formData.get(keyTitle));
+                console.log(document.querySelector('#'+previewPageTitle).textContent);/* = formData.get(keyTitle);
+                /*document.getElementById(previewPostCardTitle).textContent = formData.get(keyTitle);*/
+            };
+            /*
+            if (formData.get(keySubtitle) != "") {
+                document.getElementById(previewPageSubitle).textContent = formData.get(keySubtitle);
+                document.getElementById(previewPostCardSubtitle).textContent = formData.get(keySubtitle);
+            };
+            if (formData.get(keyAuthorName) != "") {
+                document.getElementById(previewAuthorName).textContent = formData.get(keyAuthorName);
+            };
+            if (formData.get(keyPublishDate) != "") {
+                document.getElementById(previewPublishDate).textContent = formData.get(keyPublishDate);
+            };
+            if (formData.get(keyContent) != "") {
+                document.getElementById(previewPublishDate).textContent = formData.get(keyContent);
+            };
     */
+        }
+        /*
+            function fieldKeyUpHandler(e) {
+                e.addEventListener('keyup', fieldKeyUp);
+            }
+        
+            function fieldKeyUp(e) {
+                const incorrectField = document.querySelector('#status-incorect');
+                const needcheckField = document.querySelector('#status-need_check');
+                incorrectField.target.classList.add('hide_element');
+                needcheckField.target.classList.add('hide_element');
+        
+        
+            }
+        */
 
 });
