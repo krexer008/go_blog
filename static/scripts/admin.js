f8a9537f0bfa1479ce9d38cc6f84c7706fef18e2
krexer008 <y.fedor86@mail.ru>
2023-05-27 23:40:18 +0300

    23.40

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -14,43 +14,45 @@ window.addEventListener('DOMContentLoaded', function () {
     const keyContent = "Content";
 
 
+    const formData = new Map([
+        [keyTitle, ""],
+        [keySubtitle, ""],
+        [keyAuthorName, ""],
+        [keyAuthorImage, ""],
+        [keyPublishDate, ""],
+        [keyLargeImage, ""],
+        [keyShortImage, ""],
+        [keyContent, ""],
+    ])
+
+
 
     const titleField = this.document.getElementById(keyTitle);
     const subtitleField = this.document.getElementById(keySubtitle);
-    const authorNameField = this.document.getElementById(keyAuthorName);
+    const authorNameField = this.document.getElementById(keyAuthorName);/*
     const authorImageField = this.document.getElementById(keyAuthorImage);
     const dateField = this.document.getElementById(keyPublishDate);
     const largeImageField = this.document.getElementById(keyLargeImage);
     const shortImageField = this.document.getElementById(keyShortImage);
-    const contentField = this.document.getElementById(keyContent);
+    const contentField = this.document.getElementById(keyContent);*/
 
-    var validValid = true;
+    var validFieldsValid = true;
 
     fieldViewHandler(titleField);
     fieldViewHandler(subtitleField);
-    fieldViewHandler(nameField);
+    fieldViewHandler(authorNameField);
+    
+    
+    /*
     fieldViewHandler(dateField);
-
+    
+/*
     fieldKeyUpHandler(titleField);
     fieldKeyUpHandler(subtitleField);
     fieldKeyUpHandler(nameField);
     fieldKeyUpHandler(nameField);
+*/
 
-
-    function fieldKeyUpHandler(e) {
-        e.addEventListener('keyup', fieldKeyUp);
-    }
-
-    function fieldKeyUp(e) {
-        const incorrectField = document.querySelector('#status-incorect');
-        const needcheckField = document.querySelector('#status-need_check');
-        incorrectField.target.classList.add('hide_element');
-        needcheckField.target.classList.add('hide_element');
-
-        const formRequired = e.target
-
-
-    }
 
     function fieldViewHandler(input) {
         input.addEventListener('focus', fieldFocused);
@@ -59,17 +61,62 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function fieldFocused(e) {
         e.target.classList.remove('form__field_full');
+        e.target.classList.remove('form__field_critical');
         e.target.classList.add('form__field_focused');
     }
 
     function fieldBlured(e) {
         e.target.classList.remove('form__field_focused');
-        if (e.target.value == "") {
+        if (e.target.value === "") {
             e.target.classList.remove('form__field_full');
-            e.target.classList.
         } else {
             e.target.classList.add('form__field_full');
+            this.alert('asd');
+           /* formData.set(e.target.id, e.target.value);
+            
+            /*updatePreview();*/
         }
     }
+
+    function updatePreview() {
+        if (formData.get(keyTitle) !== "") {
+            document.getElementById(previewPageTitle).textContent = formData.get(keyTitle);
+            document.getElementById(previewPostCardTitle).textContent = formData.get(keyTitle);
+        };
+        if (formData.get(keySubtitle) !== "") {
+            document.getElementById(previewPageSubitle).textContent = formData.get(keySubtitle);
+            document.getElementById(previewPostCardSubtitle).textContent = formData.get(keySubtitle);
+        };
+        if (formData.get(keyAuthorName) !== "") {
+            document.getElementById(previewAuthorName).textContent = formData.get(keyAuthorName);
+        };
+        if (formData.get(keyPublishDate) !== "") {
+            document.getElementById(previewPublishDate).textContent = formData.get(keyPublishDate);
+        };
+        if (formData.get(keyContent) !== "") {
+            document.getElementById(previewPublishDate).textContent = formData.get(keyContent);
+        };
+        /*
+        
+                [keyPublishDate, ""],
+                    [keyLargeImage, ""],
+                    [keyShortImage, ""],
+                    [keyContent, ""],
+        */
+    }
+    /*
+        function fieldKeyUpHandler(e) {
+            e.addEventListener('keyup', fieldKeyUp);
+        }
+    
+        function fieldKeyUp(e) {
+            const incorrectField = document.querySelector('#status-incorect');
+            const needcheckField = document.querySelector('#status-need_check');
+            incorrectField.target.classList.add('hide_element');
+            needcheckField.target.classList.add('hide_element');
+    
+    
+        }
+    */
 
 });
